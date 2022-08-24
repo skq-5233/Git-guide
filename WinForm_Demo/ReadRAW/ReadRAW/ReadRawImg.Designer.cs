1a36eea5744cec0cc5bad64d347745ce2cb624be
C#EXAMPLE <794919561@qq.com>
2022-08-24 18:10:35 +0800

    WinformDemo--0824

---
+++ WinForm_Demo/ReadRAW/ReadRAW/ReadRawImg.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ReadRAW
+{
+    partial class ReadRawImg
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReadRawImg));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.uiButton2_SaveImg = new Sunny.UI.UIButton();
+            this.uiButton1_Load_Img = new Sunny.UI.UIButton();
+            this.uiPanel1 = new Sunny.UI.UIPanel();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.uiLabel1 = new Sunny.UI.UILabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelControl_time = new DevExpress.XtraEditors.LabelControl();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Aside = new Sunny.UI.UINavMenu();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.uiPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // uiButton2_SaveImg
+            // 
+            this.uiButton2_SaveImg.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.uiButton2_SaveImg, "uiButton2_SaveImg");
+            this.uiButton2_SaveImg.Name = "uiButton2_SaveImg";
+            this.uiButton2_SaveImg.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton2_SaveImg.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.uiButton2_SaveImg.Click += new System.EventHandler(this.Save_Img_Click);
+            // 
+            // uiButton1_Load_Img
+            // 
+            this.uiButton1_Load_Img.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.uiButton1_Load_Img, "uiButton1_Load_Img");
+            this.uiButton1_Load_Img.Name = "uiButton1_Load_Img";
+            this.uiButton1_Load_Img.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton1_Load_Img.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.uiButton1_Load_Img.Click += new System.EventHandler(this.Load_Raw_Img_Click);
+            // 
+            // uiPanel1
+            // 
+            this.uiPanel1.Controls.Add(this.pictureBox7);
+            this.uiPanel1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            this.uiPanel1.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.uiPanel1, "uiPanel1");
+            this.uiPanel1.Name = "uiPanel1";
+            this.uiPanel1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiPanel1.StyleCustomMode = true;
+            this.uiPanel1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.uiPanel1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.pictureBox7, "pictureBox7");
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.TabStop = false;
+            // 
+            // uiLabel1
+            // 
+            this.uiLabel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.uiLabel1, "uiLabel1");
+            this.uiLabel1.Name = "uiLabel1";
+            this.uiLabel1.Style = Sunny.UI.UIStyle.Custom;
+            this.uiLabel1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.Time_Tick);
+            // 
+            // labelControl_time
+            // 
+            this.labelControl_time.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(160)))), ((int)(((byte)(255)))));
+            this.labelControl_time.Appearance.Font = ((System.Drawing.Font)(resources.GetObject("labelControl_time.Appearance.Font")));
+            this.labelControl_time.Appearance.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(48)))), ((int)(((byte)(48)))));
+            this.labelControl_time.Appearance.Options.UseBackColor = true;
+            this.labelControl_time.Appearance.Options.UseFont = true;
+            this.labelControl_time.Appearance.Options.UseForeColor = true;
+            resources.ApplyResources(this.labelControl_time, "labelControl_time");
+            this.labelControl_time.Name = "labelControl_time";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox2.Controls.Add(this.uiButton1_Load_Img);
+            this.groupBox2.Controls.Add(this.uiButton2_SaveImg);
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // Aside
+            // 
+            this.Aside.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Aside.DrawMode = System.Windows.Forms.TreeViewDrawMode.OwnerDrawAll;
+            resources.ApplyResources(this.Aside, "Aside");
+            this.Aside.FullRowSelect = true;
+            this.Aside.ItemHeight = 50;
+            this.Aside.MenuStyle = Sunny.UI.UIMenuStyle.Custom;
+            this.Aside.Name = "Aside";
+            this.Aside.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            ((System.Windows.Forms.TreeNode)(resources.GetObject("Aside.Nodes")))});
+            this.Aside.ShowLines = false;
+            this.Aside.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Aside.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // ReadRawImg
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.Controls.Add(this.Aside);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.labelControl_time);
+            this.Controls.Add(this.uiLabel1);
+            this.Controls.Add(this.uiPanel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "ReadRawImg";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ReadRawImg_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.uiPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Sunny.UI.UIButton uiButton1_Load_Img;
+        private Sunny.UI.UIButton uiButton2_SaveImg;
+        private Sunny.UI.UIPanel uiPanel1;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private Sunny.UI.UILabel uiLabel1;
+        private System.Windows.Forms.Timer timer1;
+        private DevExpress.XtraEditors.LabelControl labelControl_time;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private Sunny.UI.UINavMenu Aside;
+    }
+}
+
