7457da2060b06159e3ff600133296c3ae2c9a80d
C#EXAMPLE <794919561@qq.com>
2022-09-22 17:27:21 +0800

    C# 调用Python(exe)

--- C#_Learning/Test-Python/Test-Python/Form1.Designer.cs
+++ C#_Learning/Test-Python/Test-Python/Form1.Designer.cs
@@ -30,6 +30,9 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -52,16 +55,42 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(131, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(131, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(380, 305);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +98,9 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- C#_Learning/Test-Python/Test-Python/Form1.cs
+++ C#_Learning/Test-Python/Test-Python/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace Test_Python
 {
@@ -20,31 +21,61 @@ namespace Test_Python
         private void button1_Click(object sender, EventArgs e)
         {
 
-            if (textBox1.Text == string.Empty)
-            {
-                textBox1.Text = "0";
-            }
-            if (textBox2.Text == string.Empty)
-            {
-                textBox2.Text = "0";
-            }
+            #region C# 调用Python;
+            //if (textBox1.Text == string.Empty)
+            //{
+            //    textBox1.Text = "0";
+            //}
+            //if (textBox2.Text == string.Empty)
+            //{
+            //    textBox2.Text = "0";
+            //}
 
 
-            int num1 = Convert.ToInt32(textBox1.Text);
-            int num2 = Convert.ToInt32(textBox2.Text);
+            //int num1 = Convert.ToInt32(textBox1.Text);
+            //int num2 = Convert.ToInt32(textBox2.Text);
 
 
-            //调用python
-            ScriptRuntime pyRunTime = Python.CreateRuntime();
-            dynamic obj = pyRunTime.UseFile("C:/Users/Lee/Desktop/States/test.py");
-            label_result.Text = Convert.ToString(obj.add(num1, num2));
+            ////调用python
+            //ScriptRuntime pyRunTime = Python.CreateRuntime();
+            //dynamic obj = pyRunTime.UseFile("‪C:/Users/eivision/Desktop/test.py");
+            //label_result.Text = Convert.ToString(obj.add(num1, num2));
+            #endregion
+
+
+            //C# 调用exe;
+            label1.Text = "waiting . . .";
+            string num1;
+            if (textBox1.Text.Length != 0)
+                num1 = textBox1.Text;
+            else
+                num1 = "0";
+
+            string pyexePath = @".\test.exe";
+
+            Process p = new Process();
+            p.StartInfo.FileName = pyexePath;//需要执行的文件路径
+            p.StartInfo.UseShellExecute = false; //必需
+            p.StartInfo.RedirectStandardOutput = true;//输出参数设定
+            p.StartInfo.RedirectStandardInput = true;//传入参数设定
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.Arguments = num1;//参数以空格分隔，如果某个参数为空，可以传入””
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();//关键，等待外部程序退出后才能往下执行}
+            //Console.Write(output);//输出
+            textBox1.Text = Convert.ToString(output);
+            p.Close();
+
+            label1.Text = "finish";
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             textBox1.Text = "0";
             textBox2.Text = "0";
-            label_result.Text = "?";
+            label1.Text = "?";
         }
 
 
